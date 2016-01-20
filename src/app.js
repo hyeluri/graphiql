85c3c08fe02658255ef8be1fcde80d3ae3ef188e
Harish Yeluri <harish.yeluri@neustar.biz>
2016-01-20 07:56:24 -0800

    working graphiql

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+import {buildClientSchema} from 'graphql/'
+
+function graphQLFetcher(graphQLParams) {
+
+  var paramString = JSON.stringify(graphQLParams);
+  var posSubscription = paramString.indexOf('subscriptionType { name }')
+
+  if(posSubscription > -1){
+    paramString = paramString.substr(0,posSubscription) + paramString.substr(posSubscription + 27)
+  }
+
+  graphQLParams = JSON.parse(paramString);
+
+  return fetch('https://harish.dev.agkn.net:8443/api/v1.0/helloworld?query=' + encodeURIComponent(graphQLParams.query), {
+    method: 'get',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include'
+
+  })
+  .then(response => response.json())
+  // .then(response => {
+  //   if(!response.data){
+  //     console.log(response);
+  //     return {data: response};
+  //   }
+  //   return response;
+  // });
+  // .then(response => {
+  //   if(posSubscription > -1){
+  //     let res = JSON.parse(res);
+  //     res.data.__schema.subscriptionType = null;
+  //     return JSON.stringify(res);
+  //   }
+  //   return response;
+  // });
+}
+
+ReactDOM.render(<GraphiQL fetcher={graphQLFetcher} />, document.querySelector("#app"));
